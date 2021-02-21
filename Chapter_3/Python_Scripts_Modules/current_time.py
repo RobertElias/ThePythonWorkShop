2fa3e6be02f487f815d99574e4689c0a3678a42c
robertelias <robert.elias@live.com>
2021-02-20 17:51:23 -0800

    scripts completed

---
+++ Chapter_3/Python_Scripts_Modules/current_time.py
@@ -0,0 +1,15 @@
+
+"""
+This module prints the current time...
+"""
+
+
+import datetime
+time = datetime.datetime.now().time()
+if __name__ == '__main__':
+        print(time)
+
+# from datetime import datetime
+
+# date = datetime.now()
+# print(date)
